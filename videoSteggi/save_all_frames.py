991b97f31948f6def7116bd581d24ac0bcf45df7
Maha Maryam <119899393+mahamaryam@users.noreply.github.com>
2025-02-21 16:07:50 +0500

    dmksfnj

--- videoSteggi/save_all_frames.py
+++ videoSteggi/save_all_frames.py
@@ -2,11 +2,9 @@ import cv2
 import os
 
 def extract_frames(video_path, output_dir):
-    # Check if the output directory exists, create it if it doesn't
     if not os.path.exists(output_dir):
         os.makedirs(output_dir)
 
-    # Open the video file
     video_capture = cv2.VideoCapture(video_path)
     
     if not video_capture.isOpened():
@@ -17,21 +15,17 @@ def extract_frames(video_path, output_dir):
     while True:
         ret, frame = video_capture.read()
         if not ret:
-            break  # Exit the loop if no more frames are available
-        
-        # Save the frame as an image
+            break 
         frame_filename = os.path.join(output_dir, f"frame_{frame_number:04d}.png")
         cv2.imwrite(frame_filename, frame)
         print(f"Saved: {frame_filename}")
         
         frame_number += 1
 
-    # Release the video capture object
     video_capture.release()
     print(f"All frames extracted and saved to {output_dir}")
 
-# Usage example
-video_path = "v1.mp4"  # Replace with your video file path
-output_dir = "frames_output"   # Replace with your desired output directory
+video_path = "v1.mp4" 
+output_dir = "frames_output"  
 extract_frames(video_path, output_dir)
 
