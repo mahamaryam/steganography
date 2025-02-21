733517f55e8578c5a2b457b99fe9a0faba57561e
Maha Maryam <119899393+mahamaryam@users.noreply.github.com>
2025-02-21 22:31:19 +0500

    Update cat_dim.py

--- unresolved/giphygif/cat_dim.py
+++ unresolved/giphygif/cat_dim.py
@@ -1,8 +1,7 @@
 from PIL import Image, ImageSequence
 stego_gif = Image.open('cat.gif')
     
-    # Extract width, height, and number of frames from the first 48 bits of the first frame
-first_frame = stego_gif.convert("P")  # Ensure it's in P mode
+first_frame = stego_gif.convert("P")  
 pixels = first_frame.load()
 width, height = first_frame.size
 print("width = ",width)
